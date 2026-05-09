10ded2642cd2cba7ae809505f19f71bc6af791fa
Abeer Choubey <sentinel.loqs@proton.me>
2026-05-10 01:02:47 +0530

    Fix download link for sample contracts

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -74,7 +74,7 @@ const FileUpload = ({ onFileChange, file }) => {
         )}
       </div>
 
-      <a href="/sample-contracts.zip" className="download-sample-btn" download>
+      <a href="/sample_contracts.zip" className="download-sample-btn" download>
         <svg className="download-sample-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
           <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
           <polyline points="7 10 12 15 17 10"></polyline>
